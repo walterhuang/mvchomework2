c55ebb64aded5e11b4e78fccd9b8ffae4b4b8ec9
walterhuang <isponder@gmail.com>
2014-05-12 10:45:11 +0800

    rount constraint

--- MvcHomework2/App_Start/RouteConfig.cs
+++ MvcHomework2/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace MvcHomework2
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                constraints: new { id = @"\d+" }
             );
         }
     }
